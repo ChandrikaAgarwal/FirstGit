4f34217ff7f4810c2d98473f1d238861cab9ac43
ChandrikaAgarwal <chandrikaagarwal2@gmail.com>
2024-12-25 14:54:25 +0000

    Dynamic routes 2

---
+++ controllers/shop.js
@@ -0,0 +1,73 @@
+const Product = require('../models/product');
+const Cart = require('../models/cart');
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      prods: products,
+      pageTitle: 'All Products',
+      path: '/products'
+    });
+  });
+};
+
+exports.getProduct = (req, res, next) => {
+  const prodId = req.params.productId; //so because we have used productId as the name after the colon so we can use it on params object
+  Product.findById(prodId,product=>{
+    console.log("Get product: ",product);
+    
+  })
+  
+  Product.findById(prodId, product => {
+    console.log(prodId);
+    res.render('shop/product-detail', {
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/'
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    path: '/cart',
+    pageTitle: 'Your Cart'
+  });
+};
+
+exports.postCart= (req,res,next)=>{
+const prod_Id=req.body.ProductId //productId is the name we are using in the product-detail.ejs view file
+Product.findById(prod_Id,(product)=>{
+console.log(prod_Id);
+console.log(product);
+
+
+Cart.addProduct(prod_Id,product.price);
+})
+res.redirect('/cart') //this will go to the get cart
+
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    path: '/orders',
+    pageTitle: 'Your Orders'
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    path: '/checkout',
+    pageTitle: 'Checkout'
+  });
+};
---
+++ models/product.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      cb([]);
+    } else {
+      cb(JSON.parse(fileContent));
+    }
+  });
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id = Math.random().toString();
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id, cb) {
+    getProductsFromFile(products => {
+      console.log("id: ",id);
+      // console.log("products ",products);
+      const product = products.find(p => p.id === String(id)); //find executes the function passed to eavery element of the array
+      console.log("prod ",product);
+      
+      
+      cb(product);
+    });
+  }
+};
