594e231e7883ac770b9a2c130e42c834e1c392d3
ChandrikaAgarwal <chandrikaagarwal2@gmail.com>
2025-03-09 14:53:23 +0000

    monthly page completed

--- 1.0-Expense_Tracker/public/monthly.js
+++ 1.0-Expense_Tracker/public/monthly.js
@@ -4,10 +4,7 @@ const nextBtn = document.getElementById('nextbtn')
 const api_url = 'http://localhost:5000'
 const displayExpenses = document.querySelector('.display_expenses')
 const leaderBoard = document.querySelector('.leaderBoard a')
-const ul = document.createElement('ul')
-displayExpenses.appendChild(ul)
-ul.className = "monthly_expense_list"
-displayExpenses.appendChild(ul)
+const tableBody = document.querySelector('.monthExpenses')
 let today = new Date()
 let currentDay = today.getDate();
 let currentMonth = today.getMonth()
@@ -77,7 +74,7 @@ async function getExpensesByMonth() {
     } catch (err) {
         console.log("Error in monthly expenses:  ", err);
     }
-    displayMonthlyExpenses(monthlyResponse.data.allExpenses, monthlyResponse.data.totalInc, monthlyResponse.data.totalExp, monthlyResponse.data.carryforward, monthlyResponse.data.balance)
+    displayMonthlyExpenses(monthlyResponse.data.allExpenses, monthlyResponse.data.allincomes, monthlyResponse.data.totalInc, monthlyResponse.data.totalExp, monthlyResponse.data.carryforward, monthlyResponse.data.balance)
     if (!monthlyResponse.data.isPremium) {
         leaderBoard.style.color = "gray"
         leaderBoard.style.cursor = "not-allowed"
@@ -93,15 +90,39 @@ let totalExpenseDisplay = document.querySelector('.totalExpenseDisplay')
 let carryForwardDisplay = document.querySelector('.cfDisplay')
 let balanceDisplay = document.querySelector('.balanceDisplay')
 
-async function displayMonthlyExpenses(allExpenses, monthlyInc, monthlyExp, carryForward, balance) {
+async function displayMonthlyExpenses(allExpenses, allIncomes, monthlyInc, monthlyExp, carryForward, balance) {
     try {
-        ul.innerHTML = ""
-        for (let expense of allExpenses) {
-            const expenseLi = document.createElement('li')
-            let createdAt = expense.createdAt.split('T')[0]
-            expenseLi.textContent = `${createdAt}-${expense.description} - ${expense.category} -${expense.amount}`
-            ul.appendChild(expenseLi)
-        }
+       let mergedData=[]
+        allExpenses.forEach(expense => { 
+            mergedData.push({
+                date: expense.createdAt.split('T')[0],
+                description: expense.description,
+                category: expense.category ,
+                income: "",  // Expense ke liye income 0 rahega
+                expense: expense.amount
+            })
+        })
+
+        allIncomes.forEach(income => {
+            mergedData.push({
+                date: income.createdAt.split('T')[0],
+                income: income.amount,
+                description: income.description,
+                category: "",
+                expense:""
+            })
+        })
+        mergedData.sort((a, b) => new Date(a.date) - new Date(b.date));
+        tableBody.innerHTML = "";
+        mergedData.forEach(entry => {
+            const row = document.createElement('tr')
+            row.innerHTML = `<td>${entry.date}</td> 
+                             <td>${entry.description}</td> 
+                             <td>${entry.category}</td> 
+                             <td>${entry.income}</td> 
+                             <td>${entry.expense}</td>`;
+            tableBody.appendChild(row);
+        })
         totalIncomeDisplay.innerHTML = `<h5>${monthlyInc}</h5>`
         totalExpenseDisplay.innerHTML = `<h5>${monthlyExp}</h5>`
         carryForwardDisplay.innerHTML = `<h5>${carryForward}</h5>`
