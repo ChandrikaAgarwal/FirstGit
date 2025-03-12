f595c286eecedf7826a4cd8541a7baf47ce51e85
ChandrikaAgarwal <chandrikaagarwal2@gmail.com>
2025-03-12 19:24:06 +0000

    Made Monthly report in report section

--- 1.0-Expense_Tracker/controllers/monthlyController.js
+++ 1.0-Expense_Tracker/controllers/monthlyController.js
@@ -20,7 +20,9 @@ exports.getAllExpenses = async (req, res, next) => {
         month = parseInt(month)
         year = parseInt(year)
         let carryForward = 0;
-        let balance=0;
+        let balance = 0;
+        let totalIncome = 0;
+        let totalExpense = 0;
         console.log("month number is: ", month, "of type: ", typeof (month));
 
         const user = await User.findByPk(req.user.id)
@@ -33,46 +35,54 @@ exports.getAllExpenses = async (req, res, next) => {
         const startDate = returnedDate.startDate;
         const endDate = returnedDate.endDate
 
-        const lastExpenseDate = await Expense.findOne({
+        const existingMonth = await Month.findOne({
             where: {
                 userId: req.user.id,
-                createdAt: { [Op.lt]: new Date(`${year}-${month}`) }  // Before current month
+                monthNum: month,
+                year: year
             },
-            order: [['createdAt', 'DESC'], ['id', 'DESC']],
-            // attributes:['createdAt']
         })
 
-        console.log("Last expense date : ", lastExpenseDate)
+        // const lastExpenseDate = await Expense.findOne({
+        //     where: {
+        //         userId: req.user.id,
+        //         createdAt: { [Op.lt]: new Date(`${year}-${month}`) }  // Before current month
+        //     },
+        //     order: [['createdAt', 'DESC'], ['id', 'DESC']],
+        //     // attributes:['createdAt']
+        // })
 
-        const lastIncomeDate = await Income.findOne({
-            where: {
-                userId: req.user.id,
-                createdAt: { [Op.lt]: new Date(`${year}-${month}`) }  // Before current month
-            },
-            order: [['createdAt', 'DESC'], ['id', 'DESC']],
-            // attributes: ['createdAt']
-        })
+        // console.log("Last expense date : ", lastExpenseDate)
 
-        console.log("Last expense date: ", lastExpenseDate?.createdAt);
-        console.log("Last income date: ", lastIncomeDate?.createdAt);
+        // const lastIncomeDate = await Income.findOne({
+        //     where: {
+        //         userId: req.user.id,
+        //         createdAt: { [Op.lt]: new Date(`${year}-${month}`) }  // Before current month
+        //     },
+        //     order: [['createdAt', 'DESC'], ['id', 'DESC']],
+        //     // attributes: ['createdAt']
+        // })
 
-        if (!lastExpenseDate && !lastIncomeDate) {
-            carryForward = 0;
-        }
+        // console.log("Last expense date: ", lastExpenseDate?.createdAt);
+        // console.log("Last income date: ", lastIncomeDate?.createdAt);
 
-        let lastDate = lastExpenseDate?.createdAt || lastIncomeDate?.createdAt;
-        if (lastExpenseDate && lastIncomeDate) {
-            lastDate = lastExpenseDate.createdAt > lastIncomeDate.createdAt ? lastExpenseDate.createdAt : lastIncomeDate.createdAt;
-        }
+        // if (!lastExpenseDate && !lastIncomeDate) {
+        //     carryForward = 0;
+        // }
 
-        console.log("last Date: ", lastDate);
+        // let lastDate = lastExpenseDate?.createdAt || lastIncomeDate?.createdAt;
+        // if (lastExpenseDate && lastIncomeDate) {
+        //     lastDate = lastExpenseDate.createdAt > lastIncomeDate.createdAt ? lastExpenseDate.createdAt : lastIncomeDate.createdAt;
+        // }
 
-        if (lastDate === lastExpenseDate?.createdAt) {
-            carryForward = lastExpenseDate?.currentsaving || 0;
-        } else if (lastDate === lastIncomeDate?.createdAt) {
-            carryForward = lastIncomeDate?.totalsaving || 0;
-        }
-        console.log("carry Forward: ", carryForward);
+        // console.log("last Date: ", lastDate);
+
+        // if (lastDate === lastExpenseDate?.createdAt) {
+        //     carryForward = lastExpenseDate?.currentsaving || 0;
+        // } else if (lastDate === lastIncomeDate?.createdAt) {
+        //     carryForward = lastIncomeDate?.totalsaving || 0;
+        // }
+        // console.log("carry Forward: ", carryForward);
 
         const allexpenses = await Expense.findAll({
             where: {
@@ -90,49 +100,53 @@ exports.getAllExpenses = async (req, res, next) => {
             order: [["id", "DESC"]]
         })
 
-        let totalIncome = await Income.sum("amount", {
-            where: {
-                userId: req.user.id,
-                createdAt: { [Op.between]: [startDate, endDate] }
-            },
-        })
-        if (!totalIncome) {
-            totalIncome = 0;
-        }
+        // let totalIncome = await Income.sum("amount", {
+        //     where: {
+        //         userId: req.user.id,
+        //         createdAt: { [Op.between]: [startDate, endDate] }
+        //     },
+        // })
+        // if (!totalIncome) {
+        //     totalIncome = 0;
+        // }
 
-        let totalExpense = await Expense.sum("amount", {
-            where: {
-                userId: req.user.id,
-                createdAt: { [Op.between]: [new Date(startDate), new Date(endDate)] }
-            },
-        })
-        if (!totalExpense) {
-            totalExpense = 0;
-        }
+        // let totalExpense = await Expense.sum("amount", {
+        //     where: {
+        //         userId: req.user.id,
+        //         createdAt: { [Op.between]: [new Date(startDate), new Date(endDate)] }
+        //     },
+        // })
+        // if (!totalExpense) {
+        //     totalExpense = 0;
+        // }
 
-        if (totalExpense && totalIncome && carryForward) {
-            balance = (totalIncome + carryForward) - totalExpense
-        } else if (totalIncome && totalExpense) {
-            balance = totalIncome - totalExpense
-        } else if (totalExpense && carryForward) {
-            balance = carryForward - totalExpense
-        } else if (totalIncome && carryForward) {
-            balance = totalIncome + carryForward
-        } else if (totalExpense) {
-            balance = -totalExpense
-        } else if (carryForward) {
-            balance = carryForward
-        } else if (totalIncome) {
-            balance = totalIncome
-        }
+        // if (totalExpense && totalIncome && carryForward) {
+        //     balance = (totalIncome + carryForward) - totalExpense
+        // } else if (totalIncome && totalExpense) {
+        //     balance = totalIncome - totalExpense
+        // } else if (totalExpense && carryForward) {
+        //     balance = carryForward - totalExpense
+        // } else if (totalIncome && carryForward) {
+        //     balance = totalIncome + carryForward
+        // } else if (totalExpense) {
+        //     balance = -totalExpense
+        // } else if (carryForward) {
+        //     balance = carryForward
+        // } else if (totalIncome) {
+        //     balance = totalIncome
+        // }
 
-        console.log("balance:: ",balance);
+        // console.log("balance:: ",balance);
         
-        console.log("Filtered Expenses: ", allexpenses);
-        console.log("total Income: ", totalIncome);
-        console.log("total Expense: ", totalExpense);
- 
-        
+        // console.log("Filtered Expenses: ", allexpenses);
+        // console.log("total Income: ", totalIncome);
+        // console.log("total Expense: ", totalExpense);
+        if (existingMonth) {
+            totalIncome = existingMonth.totalIncome
+            totalExpense = existingMonth.totalExpense
+            balance = existingMonth.balance
+            carryForward = existingMonth.carryForward
+        }
         res.status(200).json({ message: "getting all expenses", allExpenses: allexpenses, allincomes: allincomes, totalInc: totalIncome, totalExp: totalExpense, carryforward: carryForward, balance: balance,isPremium:user.premium })
     } catch (err) {
         console.log("Error in getting all expenses: ", err);
--- 1.0-Expense_Tracker/public/report.js
+++ 1.0-Expense_Tracker/public/report.js
@@ -1,4 +1,3 @@
-
 const api_url = 'http://localhost:5000'
 const token = localStorage.getItem('token')
 const reportForm = document.querySelector("#reportForm")
@@ -7,6 +6,7 @@ const yearSelect = document.getElementById("yearSelect");
 const submitBtn = document.querySelector(".submitBtn")
 const h5Year = document.querySelector("#year")
 const h5Month = document.querySelector("#month")
+const tableBody = document.querySelector('.displayReport')
 let reportDetails;
 for (let i = 0; i < 12; i++) {
     const monthName = new Date(2000, i).toLocaleString('en-US', { month: 'long' });
@@ -23,6 +23,7 @@ years.forEach(year => {
     yearSelect.appendChild(option);
 });
 
+
 reportForm.addEventListener("submit", async (e) => {
     e.preventDefault();
     reportDetails={
@@ -30,35 +31,90 @@ reportForm.addEventListener("submit", async (e) => {
         year : e.target.yearSelect.value,
     }
     console.log("report Details: ", reportDetails);
-    localStorage.setItem("reportDetail", reportDetails)
-    const monthIndex = parseInt(reportDetails.month, 10) - 1; //converts month number from string to integer
-    const monthName= new Date(2000,monthIndex).toLocaleString('en-US',{month:"long"})
-    h5Year.textContent = `${reportDetails.year}`
-    h5Month.textContent = `${monthName} ${reportDetails.year}`
-    localStorage.setItem("selectedReportMonth", monthName);
-    localStorage.setItem("selectedReportYear", reportDetails.year);
-   await getMonthReport(reportDetails)
-})
-
-window.addEventListener("DOMContentLoaded", async () => {
-    const savedMonth = localStorage.getItem("selectedReportMonth");
-    const savedYear = localStorage.getItem("selectedReportYear");
-    if (savedMonth && savedYear) { 
-        h5Year.textContent = `${savedYear}`
-        h5Month.textContent = `${savedMonth} ${savedYear}`
-    }
+    await getMonthReport(reportDetails.year, reportDetails.month)
+    h5Year.textContent = reportDetails.year
+    let monthIndex = parseInt(reportDetails.month)
+    console.log("monthIndex: ",monthIndex);
+    
+    h5Month.textContent = new Date(2000, monthIndex - 1).toLocaleString('en-US', { month: 'long' }) + " " + reportDetails.year;
    
 })
 
-async function getMonthReport(repDetail) {
+window.addEventListener("DOMContentLoaded", async () => {
+    let today = new Date();
+    let currentMonth = today.getMonth()+1
+    let freshYear = today.getFullYear()
+    yearSelect.value = freshYear
+    monthSelect.value = String(currentMonth).padStart(2, "0"); 
+    h5Year.textContent = freshYear
+    h5Month.textContent = monthSelect.options[monthSelect.selectedIndex].text+" "+freshYear
+    await getMonthReport(freshYear,currentMonth)
+    
+   
+})
+
+async function getMonthReport(year,month) {
     try {
-        const monthReport = await axios.get(`${api_url}/api/monthly/report/?month=${repDetail.month}&year=${repDetail.year}`, {
+        const monthReport = await axios.get(`${api_url}/api/monthly/report/?month=${month}&year=${year}`, {
             headers: {
                 "Authorization": `Bearer ${token}`
             }
         })
         console.log("Getting month Report: ", monthReport);
+        displayMonthlyReport(monthReport.data.allExpenses, monthReport.data.allincomes, monthReport.data.totalInc, monthReport.data.totalExp, monthReport.data.carryforward,monthReport.data.balance)
     } catch (error) { 
         console.log("Error getting monthly report:  ", error);
+    }
+}
+
+async function displayMonthlyReport(allExpenses, allIncomes, monthlyInc, monthlyExp, carryForward, balance) {
+    try {
+        let mergedData = []
+        allExpenses.forEach(expense => {
+            mergedData.push({
+                date: expense.createdAt.split('T')[0],
+                description: expense.description,
+                category: expense.category,
+                income: "",  // Expense ke liye income 0 rahega
+                expense: expense.amount
+            })
+        })
+
+        allIncomes.forEach(income => {
+            mergedData.push({
+                date: income.createdAt.split('T')[0],
+                income: income.amount,
+                description: income.description,
+                category: "",
+                expense: ""
+            })
+        })
+        mergedData.sort((a, b) => new Date(a.date) - new Date(b.date));
+        tableBody.innerHTML = "";
+        mergedData.forEach(entry => {
+            const row = document.createElement('tr')
+            row.innerHTML = `<td>${entry.date}</td> 
+                             <td>${entry.description}</td> 
+                             <td>${entry.category}</td> 
+                             <td>${entry.income}</td> 
+                             <td>${entry.expense}</td>`;
+            tableBody.appendChild(row);
+        })
+        tableBody.innerHTML += `
+    <tr style="font-weight: bold; background-color: #333; color: white;">
+        <td colspan="3">Total</td>
+        <td>${monthlyInc.toFixed(2)}</td>
+        <td>${monthlyExp.toFixed(2)}</td>
+    </tr>
+    <tr style="font-weight: bold; background-color: #333; color: white;">
+    <td colspan="4">Carry Forward (C/F)</td>
+        <td>${carryForward.toFixed(2)}</td>
+    </tr>
+    <tr style="font-weight: bold; background-color: #444; color: lightgreen;">
+        <td colspan="4">Savings</td>
+        <td>${balance.toFixed(2)}</td>
+    </tr>`;
+    } catch (err) {
+        console.log("error displaying monthly report: ", err);
     }
 }
