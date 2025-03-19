8066bf22692b5fd9dd90f1397accb82b8ac17242
ChandrikaAgarwal <chandrikaagarwal2@gmail.com>
2025-03-19 08:39:19 +0000

    modified app.js

--- 1.0-Expense_Tracker/app.js
+++ 1.0-Expense_Tracker/app.js
@@ -88,7 +88,7 @@ User.hasMany(FileUrl, { constraints: true, onDelete: 'CASCADE' })
 sequelize.sync()
     .then(() => {
         app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
-            console.log("Server running on  http://0.0.0.0:5000");
+            console.log("Server running on  http://localhost:5000");
 
         })
     }).catch(err => console.log(err))
