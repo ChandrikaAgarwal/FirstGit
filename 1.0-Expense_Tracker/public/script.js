3b8d4c14e027681863116cd48009dd595ca123f3
ChandrikaAgarwal <chandrikaagarwal2@gmail.com>
2025-03-22 03:06:43 +0000

    corrected a mistake

--- 1.0-Expense_Tracker/public/script.js
+++ 1.0-Expense_Tracker/public/script.js
@@ -44,7 +44,7 @@ if (signupForm) {
             phone: e.target.phone.value,
             password: e.target.password.value
         }
-        console.log("phone length: ",typeof(phone.length));
+        console.log("phone length: ",typeof(newuserDetail.phone.length));
         
         if (phone.length !== 10) {
             console.log("entering if");
