905250e2ce0c38f26433a85bfe92b51c3d0c2faa
ChandrikaAgarwal <chandrikaagarwal2@gmail.com>
2025-03-24 15:54:43 +0000

    changed back to previous

--- 1.0-Expense_Tracker/public/script.js
+++ 1.0-Expense_Tracker/public/script.js
@@ -9,8 +9,7 @@ const togBtn = document.getElementById("toggle")
 const cat = document.getElementById("category")
 const catlabel = document.getElementById("catlabel")
 // const api_url = 'http://15.206.27.247'
-// const api_url = "http://15.206.27.247";
-const api_url="http://127.0.0.1:5000"
+const api_url = "http://15.206.27.247";
 
 const containerfluid = document.querySelector('.expense-Div')
 const container = document.querySelector('.savingsandIncome')
@@ -66,7 +65,7 @@ if (signupForm) {
         } catch (err) {
             console.log("error in signing up: ", err);
 
-            console.error("Error: ", err)
+            console.error("Error: ", err.response)
             if (err.response && err.response.data.message) {
                 alert(err.response.data.message)
                 if (err.response.data.message === "User already exists.Please log in.") {
