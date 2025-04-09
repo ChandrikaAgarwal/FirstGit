487d41f44461ea1b04812aeaccf89cc82ba13c9f
ChandrikaAgarwal <chandrikaagarwal2@gmail.com>
2025-04-09 14:45:56 +0000

    checking git

--- NodeTestProject/public/script.js
+++ NodeTestProject/public/script.js
@@ -56,7 +56,7 @@ if (loginForm) {
             const loginRes = await axios.post(`${api_url}/users`, user)
             console.log("login response: ", loginRes);
             localStorage.setItem("token", loginRes.data.token)
-            alert("Login successful")
+            alert("Login successful!!")
             loginForm.reset()
             // window.location.href = "/chat"
         } catch (err) {
